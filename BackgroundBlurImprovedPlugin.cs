4ce15d6426356fdf740ff5e896427e140d3298d8
PixelKiri <alexchirila87@gmail.com>
2025-10-10 08:43:38 +0300

    Configurable resolution

--- BackgroundBlurImprovedPlugin.cs
+++ BackgroundBlurImprovedPlugin.cs
@@ -11,8 +11,29 @@ namespace BackgroundBlurImproved;
 public partial class BackgroundBlurImprovedPlugin : BaseUnityPlugin {
     private readonly Harmony harmony = new(Id);
 
+    public enum BlurHeight {
+        res2160 = 2160,
+        res1440 = 1440,
+        res1080 = 1080,
+        res720 = 720,
+        res576 = 576,
+        res540 = 540,
+        res432 = 432,
+        res360Default = 360,
+        res270 = 270,
+        res240 = 240,
+        res216 = 216,
+    }
+    public static ConfigEntry<BlurHeight> BaseHeightConfig;
+
     private void Awake() {
         Logger.LogInfo($"Plugin {Name} ({Id}) has loaded!");
+        BaseHeightConfig = Config.Bind(
+            "General",
+            "BackgroundBlur layer height",
+            BlurHeight.res360Default,
+            "The height of the BackgroundBlur layer. Quit & reload is required in order to apply the changes."
+        );
         harmony.PatchAll();
     }
 
@@ -20,21 +41,9 @@ public partial class BackgroundBlurImprovedPlugin : BaseUnityPlugin {
     public class PatchedBlurManager_Awake {
         [HarmonyPrefix]
         static void Prefix(BlurManager __instance) {
-            var adjustedBaseHeight = 540;
+            var adjustedBaseHeight = (int)BaseHeightConfig.Value;
             Debug.Log($"BlurManager.Awake() called on {__instance} hash:{__instance.GetHashCode()} baseHeight:{__instance.baseHeight} -> {adjustedBaseHeight}");
             __instance.baseHeight = adjustedBaseHeight;
-            /*
-            1	2160
-            2	1080
-            3	720
-            4	540
-            5	432
-            6	360
-            7	308.5714286
-            8	270
-            9	240
-            10	216
-            */
         }
     }
 
