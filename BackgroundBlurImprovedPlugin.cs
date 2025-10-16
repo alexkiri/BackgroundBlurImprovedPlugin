939d3e6645cf7f6db9d174fdd06a0c7df80eecf4
PixelKiri <alexchirila87@gmail.com>
2025-10-16 18:52:37 +0300

    Add presets as Buttons, code cleanup, reduce number of options

--- BackgroundBlurImprovedPlugin.cs
+++ BackgroundBlurImprovedPlugin.cs
@@ -1,6 +1,5 @@
 using BepInEx;
 using BepInEx.Configuration;
-using GenericVariableExtension;
 using HarmonyLib;
 using UnityEngine;
 
@@ -23,53 +22,105 @@ public partial class BackgroundBlurImprovedPlugin : BaseUnityPlugin {
         blurRenderTextureHeightConfigEntry = Config.Bind(
             "General",
             "RenderTextureHeight",
-            BlurHeight.High_720,
-            "The height of the BlurredBackground layer. The game default is 360. Low impact on performance."
+            BlurHeight.Medium_720,
+            new ConfigDescription(
+                "The height of the BlurredBackground layer. Improves the quality, but lowers the blur effect intensity, with low impact on performance. Game default is 360.",
+                null,
+                new ConfigurationManagerAttributes { Order = 3 }
+            )
         );
         blurRenderTextureHeightConfigEntry.SettingChanged += (sender, args) => {
             var newValue = (int)blurRenderTextureHeightConfigEntry.Value;
+            var blurEnable = blurEnableConfigEntry.Value;
             Logger.LogInfo($"blurRenderTextureHeightConfigEntry.SettingChanged -> {newValue}");
             if (lightBlurredBackground != null) {
                 Logger.LogInfo($"will set RenderTextureHeight for {lightBlurredBackground} {lightBlurredBackground.renderTextureHeight} -> {newValue}");
-                blurEnableConfigEntry.Value = true;
+                lightBlurredBackground.lightBlur.enabled = true;
                 lightBlurredBackground.RenderTextureHeight = newValue;
                 lightBlurredBackground.enabled = false;
                 lightBlurredBackground.enabled = true;
+                lightBlurredBackground.lightBlur.enabled = blurEnable;
             }
         };
+        
         blurPassGroupCountConfigEntry = Config.Bind(
             "General",
             "PassGroupCount",
             4,
             new ConfigDescription(
-                "The number of passes of the BlurredBackground layer. The game default is 2. Medium impact on performance.",
-                new AcceptableValueRange<int>(1, 64)
+                "The number of passes of the BlurredBackground layer. Increases the intensity of the blur effect, with medium / heavy impact on performance. Game default is 2.",
+                new AcceptableValueRange<int>(1, 64),
+                new ConfigurationManagerAttributes { Order = 2 }
             )
         );
         blurPassGroupCountConfigEntry.SettingChanged += (sender, args) => {
-            var newValue = (int)blurPassGroupCountConfigEntry.Value;
+            var newValue = blurPassGroupCountConfigEntry.Value;
+            var blurEnable = blurEnableConfigEntry.Value;
             Logger.LogInfo($"blurPassGroupCountConfigEntry.SettingChanged -> {newValue}");
             if (lightBlurredBackground != null) {
                 Logger.LogInfo($"will set PassGroupCount for {lightBlurredBackground} {lightBlurredBackground.passGroupCount} -> {newValue}");
-                blurEnableConfigEntry.Value = true;
+                lightBlurredBackground.lightBlur.enabled = true;
                 lightBlurredBackground.PassGroupCount = newValue;
                 lightBlurredBackground.enabled = false;
                 lightBlurredBackground.enabled = true;
+                lightBlurredBackground.lightBlur.enabled = blurEnable;
             }
         };
+        
         blurEnableConfigEntry = Config.Bind(
             "General",
             "EnableEffect",
             true,
-            "When disabled, the BlurredBackground effect is completely removed, and the other settings have no effect"
+            new ConfigDescription(
+                "When disabled, the blur effect is completely removed, and the PassGroupCount setting will have no effect.",
+                null,
+                new ConfigurationManagerAttributes { IsAdvanced = true, Order = 1 }
+            )
         );
         blurEnableConfigEntry.SettingChanged += (sender, args) => {
-            var blurEnable = (bool)blurEnableConfigEntry.Value;
+            var blurEnable = blurEnableConfigEntry.Value;
             Debug.Log($"blurEnableConfigEntry.SettingChanged -> {blurEnable}");
             if (lightBlurredBackground != null) {
                 lightBlurredBackground.lightBlur.enabled = blurEnable;
             }
         };
+
+        Config.Bind(
+            "General",
+            "Apply Presets",
+            "",
+            new ConfigDescription(
+                "Click to apply any preset.",
+                null,
+                new ConfigurationManagerAttributes { HideDefaultButton = true, CustomDrawer = PresetsCustomDrawer, Order = 0 }
+            )
+        );
+    }
+
+    private void PresetsCustomDrawer(ConfigEntryBase configEntry) {
+        if (GUILayout.Button("Vanilla", GUILayout.ExpandWidth(true))) {
+            blurEnableConfigEntry.Value = true;
+            blurRenderTextureHeightConfigEntry.Value = BlurHeight.Vanilla_360;
+            blurPassGroupCountConfigEntry.Value = 2;
+        }
+
+        if (GUILayout.Button("Medium", GUILayout.ExpandWidth(true))) {
+            blurEnableConfigEntry.Value = true;
+            blurRenderTextureHeightConfigEntry.Value = BlurHeight.Medium_720;
+            blurPassGroupCountConfigEntry.Value = 4;
+        }
+
+        if (GUILayout.Button("High", GUILayout.ExpandWidth(true))) {
+            blurEnableConfigEntry.Value = true;
+            blurRenderTextureHeightConfigEntry.Value = BlurHeight.High_1080;
+            blurPassGroupCountConfigEntry.Value = 6;
+        }
+        
+        if(GUILayout.Button("VeryHigh", GUILayout.ExpandWidth(true))) {
+            blurEnableConfigEntry.Value = true;
+            blurRenderTextureHeightConfigEntry.Value = BlurHeight.VeryHigh_1440;
+            blurPassGroupCountConfigEntry.Value = 8;
+        }
     }
 
     [HarmonyPatch]
@@ -85,7 +136,7 @@ public partial class BackgroundBlurImprovedPlugin : BaseUnityPlugin {
         [HarmonyPostfix]
         static void LightBlurredBackground_Awake(LightBlurredBackground __instance) {
             var renderTextureHeight = (int)blurRenderTextureHeightConfigEntry.Value;
-            var passGroupCount = (int)blurPassGroupCountConfigEntry.Value;
+            var passGroupCount = blurPassGroupCountConfigEntry.Value;
 
             Debug.Log($"LightBlurredBackground.Awake called on {__instance} hash:{__instance.GetHashCode()}, renderTextureHeight: {__instance.renderTextureHeight} -> {renderTextureHeight}, passGroupCount: {__instance.passGroupCount} -> {passGroupCount}");
             __instance.passGroupCount = passGroupCount;
--- BlurHeight.cs
+++ BlurHeight.cs
@@ -4,9 +4,8 @@ public enum BlurHeight {
     VeryLow_240 = 240,
     Low_270 = 270,
     Vanilla_360 = 360,
-    Medium_540 = 540,
-    High_720 = 720,
-    VeryHigh_1080 = 1080,
-    UltraHigh_1440 = 1440,
-    Insane_2160 = 2160
+    Medium_720 = 720,
+    High_1080 = 1080,
+    VeryHigh_1440 = 1440,
+    UltraHigh_2160 = 2160
 }
